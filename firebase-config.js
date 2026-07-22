993f1aeac7b741ea347fdc42fa008ba1b13cb028
lisavollugi-tech <lisa.vollugi@gmail.com>
2026-07-22 18:20:34 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD48UYccpepGcEYd6DA4KCAdNnPqXHoySI",
   authDomain: "sjnc-kites.firebaseapp.com",
